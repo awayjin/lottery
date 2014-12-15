322001f8ed41b1b806174d8acc360bcee2824e19
unknown <jinwei@3100-JINWEI1.winnermedical.com>
2014-12-15 11:10:29 +0800

    setup change

--- js/lottery/lottery.js
+++ js/lottery/lottery.js
@@ -40,7 +40,7 @@ define([
             this.ruleWrap = ".lot-draw-intro"; //抽奖规则背景框;
 
             this.ruleObj = params.ruleObj;
-//            this.data = params.data || data;
+            this.data = params.data || data;
             this.url = params.url;
             this.verify = params.verify;
 
@@ -50,9 +50,10 @@ define([
 
 
             //数据和抽奖规则初始化
-//            this.dom(this.data);
+            this.dom(this.data);
 
             var that = this;
+            //开始
             $(this.startBtn).click(function(){
                 that.clickStart(that);
             });
@@ -94,6 +95,10 @@ define([
             return this;
         },
 
+        posIndex: function(){
+
+        },
+
         start: function(){
 
             var that = this,
@@ -101,9 +106,8 @@ define([
                 imgMask = this.imgMask,
                 active = this.active,
                 start = this.startBtn,
-                cyycleSuccess = this.success,
+                cycleSuccess = this.success,
 
-                url = this.url,
                 timeId, //用它存放setinterval,不用扣得太细，因为只要用到setinterval最后就得用     window.clearInterval(t);去释放他
                 index = 1,//这个用来记录当前循环的次数，比如点了抽奖要转四圈，每转一圈有10个奖品，那一共就要循环10*圈数
                 circles = Math.round(Math.random() * 10 + 5),//抽奖要转的圈数
@@ -112,16 +116,22 @@ define([
                 endPoint = that.endPoint; //最后停止的位置，即选中了谁
 
 
-            //终结点，生成1-10之内的随机数,因为你一圈有10个奖品
-            endPoint = this.endPoint;
-
             //删除活动层
-            $(active).remove();
+            //$(active).remove();
             $(start).attr("disabled", true); //禁用点击按钮以免多次点击
 
-            //开始循环
             autoScroll();
 
+            //查找活动层的位置
+            function indexActive(){
+                var iIndex = 0;
+                $(imgMask).each(function(){
+                    if($(this).find(active).length > 0){
+                        iIndex = $(this).index();
+                    }
+                });
+                return iIndex
+            }
             //循环奖品方法
             function autoScroll() {
                 if (index > 1) {
@@ -133,8 +143,23 @@ define([
                     }
                 }
 
-                var aa = index - (10*(currentCircle-1));
-                $(imgMask).eq(aa-1).append(mask);
+                //继续抽奖从上次的终结点开始
+                if($(active).length > 0){
+                    var iIndex = indexActive();
+                    if(iIndex != 0){
+                        $(active).remove();
+                        if(iIndex == 9){
+                            $(imgMask).eq(0).append(mask);
+                        }else{
+                            //iIndex+1要在这里，不然最终终结点会不对的
+                            $(imgMask).eq(iIndex+1).append(mask);
+                        }
+                    }
+                }else{
+                    var aa = index - (10*(currentCircle-1));
+                    $(imgMask).eq(aa-1).append(mask);
+                }
+
 
                 if ((index % 10) == 0) {
                     currentCircle++;
@@ -144,7 +169,7 @@ define([
                     window.clearInterval(timeId);
                     speed -= 80;
                     timeId = setInterval(autoScroll, speed);
-                }else if (currentCircle == (circles-1) ) {//倒数二圈的时候开始减速
+                }else if (currentCircle == (circles-1) ) {// circles-1倒数二圈的时候开始减速
                     window.clearInterval(timeId);
                     speed += 60;
                     timeId = setInterval(autoScroll, speed);
@@ -153,28 +178,11 @@ define([
                 if (circles == currentCircle && ((index - 1) % 10) == that.endPoint) {
                     window.clearInterval(timeId);
                     $(start).attr("disabled", false);
-                    //后台验证
-                    if(url !== undefined){
-                        $.ajax({
-                            async: true,
-                            url: url,
-                            data: endPoint,
-                            success: function(d){
-                                cyycleSuccess(that);
-                            }
-                        });
-                    }else{
-                        var prize = $(imgMask).eq(that.endPoint).find("img").attr("alt");
-                        //dialog.alert("恭喜抽中了" + prize + "号奖品");
-                        cyycleSuccess(that);
-
-                    }
+                    cycleSuccess(that);
                     return;
-
                 }
 
             }
-
 
             return this;
         }
@@ -184,197 +192,3 @@ define([
 
     return Roulette;
 });
-
-
-/**
- * @function Roulette
- * params {object} {}对象字面量
- * param.endPoint 终结点
- * */
-/*
-define([
-//    "./lib/jquery"
-    "jquery",
-//    "data",
-    "dialog"
-], function($,  dialog){
-//    var $$ = window.Roulette = function(obj){
-
-
-    var Roulette = function(obj){
-        return new Roulette.prototype.init(obj);
-    };
-    Roulette.fn = Roulette.prototype = {
-        init: function(obj){
-
-            //设置默认参数
-            var params = {
-                number: 1, //抽奖次数,默认1次
-                endPoint: Math.round(Math.random() * 10), //所中奖品
-                ruleObj: ".rule-txt", //抽奖规则包裹元素，添加天这个元素里面
-//                ruleData: data.ruleData //抽奖规则
-                wrap: ".lottery-draw",
-                lotteryBg: "#e4015f", //轮盘默认背景颜色 #e4015f
-                verify: false //后台验证。默认关闭
-            };
-
-            //合并参数
-            $.fn.extend(params, obj);
-
-            this.wrap = params.wrap;
-            this.lotteryWrap = params.lotteryWrap
-            this.number = params.number; //抽奖次数
-            this.endPoint = params.endPoint
-
-            this.mask = '<div class="lottery-prize-active"></div>'; //遮罩层
-            this.imgMask = ".lottery-draw-img";
-            this.startBtn = "#btnrun";
-            this.active = ".lottery-prize-active"; //活动层;
-            this.ruleWrap = ".lot-draw-intro"; //抽奖规则背景框;
-
-            this.ruleObj = params.ruleObj;
-//            this.data = params.data || data;
-
-            this.url = params.url;
-            this.verify = params.verify;
-
-
-            //数据和抽奖规则初始化
-//            this.dom(this.data);
-//            this.start();
-        },
-
-        //DOM初始化
-        dom: function(data){
-
-            var rule = data.ruleData,//规则
-                prize = data.prize, //奖品
-                prizeBox = this.imgMask,
-                img = "";
-
-            for(var i=0; i<prize.length; i++){
-                img = "<img src='"+prize[i]["src"]+"' alt='"+prize[i]["alt"]+"'> ";
-                $(prizeBox).eq(i).append(img);
-            }
-
-            $(this.wrap).css("background-color", data.wrapBg);
-            $(this.ruleWrap).css("background-color", data.ruleBg);
-            $(this.wrap + " a").css("color", data.linkColor);
-            $(this.wrap + " a").css("color", data.linkColor);
-
-            //抽奖规则
-            $(this.ruleObj).empty().append(rule);
-
-            //开始按钮 换背景图片
-            $(this.startBtn).css({"background-image": "url("+data.startBg+")"});
-
-            //数据加载完成后显示抽奖
-            $(this.wrap).show();
-
-        },
-
-        start: function(){
-
-            //遮罩层相关
-            var  that = this,
-                mask = that.mask,
-                imgMask = that.imgMask,
-                active = that.active,
-                start = that.startBtn,
-
-                url = that.url,
-                timeId, //用它存放setinterval,不用扣得太细，因为只要用到setinterval最后就得用     window.clearInterval(t);去释放他
-                index = 1,//这个用来记录当前循环的次数，比如点了抽奖要转四圈，每转一圈有10个奖品，那一共就要循环10*圈数
-                circles = 1,//抽奖要转的圈数
-                currentCircle = 1, //记录当前是第几圈setInterval的第二个参数，通过操纵他来实现变速
-                speed = 0,//转动的速度,用它作为
-                endPoint = that.endPoint; //最后停止的位置，即选中了谁
-
-            function startup() {
-                $(active).remove();
-                $(start).attr("disabled", true);
-                /////随机生成圈数，要跑几圈，不要太少了，太少了看着就不舒服。这里是生成4-14以内的随机整数
-                circles = Math.round(Math.random() * 10 + 5);
-                /////终结点，生成1-10之内的随机数,因为你一圈有10个奖品
-                endPoint = this.endPoint;
-
-                ////当前的圈数
-                currentCircle = 1;
-                ////速度，谁的太高了会很慢
-                speed = 800;
-                ////当前循环的次数
-                index =1;
-                /////取得放奖品的<td>的数组
-                var goods = document.getElementsByName("roundelgoods");
-                ////存放奖品的<td>的数组
-                var drawturn = [];
-
-                ////开始循环
-                autoScroll();
-                //timeId= setInterval(autoScroll,speed);
-            }
-
-            ///循环奖品方法
-            function autoScroll() {
-                if (index > 1) {
-                    if ((index % 10) == 1) {
-                        $(imgMask).eq(9).find(active).remove();
-                    } else {
-                        var bb = index - (10*(currentCircle -1)) -1;
-                        $(imgMask).eq(bb-1).find(active).remove();
-                    }
-                }
-
-                var aa = index - (10*(currentCircle-1));
-                $(imgMask).eq(aa-1).append(mask);
-
-                if ((index % 10) == 0) {
-                    currentCircle++;
-                }
-                index++;
-                if (currentCircle == 1) {
-                    window.clearInterval(timeId);
-                    speed -= 80;
-                    timeId = setInterval(autoScroll, speed);
-                }else if (currentCircle == (circles-1) ) {//倒数二圈的时候开始减速
-                    window.clearInterval(timeId);
-                    speed += 60;
-                    timeId = setInterval(autoScroll, speed);
-                }
-
-                if (circles == currentCircle && ((index - 1) % 10) == that.endPoint) {
-                    window.clearInterval(timeId);
-                    $(start).attr("disabled", false);
-                    //后台验证
-                    if(url !== undefined){
-                        $.ajax({
-                            async: true,
-                            url: url,
-                            data: endPoint,
-                            success: function(d){
-                                dialog.alert(d);
-                            }
-                        });
-                    }else{
-                        var prize = $(imgMask).eq(that.endPoint).find("img").attr("alt")
-                        dialog.alert("恭喜抽中了" + prize + "号奖品");
-                    }
-                    return;
-
-                }
-
-            }
-
-             startup();
-
-        }
-
-    };
-    Roulette.fn.init.prototype = Roulette.fn;
-
-    return Roulette;
-});
-
-
-
-*/
--- js/mainLottery.js
+++ js/mainLottery.js
@@ -14,60 +14,35 @@ require([
     "data",
     "dialog"
 ], function($, lottery, data, dialog){
-
     //抽奖配置参数
-    var obj = {
-        endPoint: 3, //终结点，就所中的奖品号
-        number: 1, //抽奖次数
-        //抽奖完成后的回调函数
-        success: function(params){
-            var prize = $(params.imgMask).eq(params.endPoint).find("img").attr("alt")
-            dialog.alert("恭喜抽中了" + prize + "号奖品");
-        },
-        //点击开始
-        clickStart: function(pararms){
-            if(pararms.number > 0){
-                pararms.number--;
-                pararms.start();//开始
-            }else{
-                dialog.alert({
-                    number: obj.number,
-                    prizeText: "您的抽奖次数已经用完!"
-                });
+    var setup = {
+            data: data, //数据初始化
+            endPoint: 3, //终结点，就所中的奖品号
+            number: 2, //抽奖次数
+            //抽奖完成后的回调函数
+            success: function(params){
+                var prize = $(params.imgMask).eq(params.endPoint).find("img").attr("alt")
+                dialog.alert("恭喜抽中了" + prize + "号奖品");
+            },
+            //点击开始
+            clickStart: function(pararms){
+                if(pararms.number > 0){
+                    pararms.number--;
+                    pararms.start();//开始
+                }else{
+                    dialog.alert({
+                        number: 0,
+                        prizeText: "您的抽奖次数已经用完!"
+                    });
+                }
+            },
+            //点击继续抽奖按钮
+            continue: function(params){
+                $(".dialog-outer").parent("div").remove();
+                $(".d-mask").remove();
+                params.clickStart(params);
             }
-        },
-        //点击继续抽奖按钮
-        continue: function(params){
-            $(".dialog-outer").parent("div").remove();
-            $(".d-mask").remove();
-            params.clickStart(params);
-        }
     };
-
     //初始化数据
-    lottery(obj).dom(data);
-
-    //点击抽奖按钮
-//    $("#btnrun").on("click", function(){
-//        if(obj.number > 0){
-//            lottery(obj).start(); //动画开始
-//            obj.number--
-//        }else{
-//            dialog.alert({
-//                number: obj.number,
-//                prizeText: "您的抽奖次数已经用完!"
-//            });
-//        }
-//    });
-
-
-     //点击继续抽奖按钮
-//    $(document).on("click", ".dialog-ok", function(){
-//        $(".dialog-outer").parent("div").remove();
-//        $(".d-mask").remove();
-//        $("#btnrun").click();
-//    });
-
-
+    lottery(setup);
 });
-
